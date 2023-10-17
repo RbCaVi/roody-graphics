d6057e22db01300356c36b5a223a5f6a8d3d8155
RbCaViTerminal <robert@robertvail.info>
2023-10-17 11:13:09 -0400

    init

---
+++ block.py
@@ -0,0 +1,209 @@
+import PIL
+import PIL.Image
+
+class Layer:
+	def __init__(self,file,x,y,tilesize):
+		im=PIL.Image.open(file)
+		self.image=im.crop((x*tilesize,y*tilesize,(x+1)*tilesize,(y+1)*tilesize))
+
+class Block:
+	def __init__(self,layers):
+		d
+
+	def draw(self):
+		im=PIL.Image.new('RGBA',(self.size,self.size),(0,0,0,0))
+		for layer in self.layers:
+			im.alpha_composite(layer.image)
+		return im
+
+#welded=top,left,bottom,right
+#rotate= 0    1    2      3
+
+class NormalBlock:
+	def __init__(self,file,offset=0):
+		self.image=PIL.Image.open(f'blocks/{file}.png').crop((offset,0,offset+32,32));
+
+	def draw(self,welded,rotate=0):
+		top,left,bottom,right=rotatewelded(welded,rotate)
+		im=PIL.Image.new('RGBA',(16,16),(0,0,0,0))
+		for x,xside in [(0,left),(8,right)]:
+			for y,yside in [(0,top),(8,bottom)]:
+				im.alpha_composite(self.image.crop((x+16*xside,y+16*yside,x+16*xside+8,y+16*yside+8)),(x,y))
+		im=rotateblock(im,rotate)
+		return im.resize((128,128),PIL.Image.NEAREST)
+
+def rotateblock(im,rotate):
+	if rotate==0:
+		return im
+	if rotate==3:
+		return im.transpose(PIL.Image.ROTATE_270)
+	if rotate==1:
+		return im.transpose(PIL.Image.TRANSPOSE)
+	if rotate==2:
+		return im.transpose(PIL.Image.FLIP_TOP_BOTTOM)
+
+def rotatewelded(welded,rotate):
+	if rotate==0:
+		return welded
+	if rotate==3:
+		return [welded[i] for i in [3,0,1,2]]
+	if rotate==1:
+		return [welded[i] for i in [1,0,3,2]]
+	if rotate==2:
+		return [welded[i] for i in [2,1,0,3]]
+
+class TwoSideBlock:
+	def __init__(self,file,offset=0):
+		self.image=PIL.Image.open(f'blocks/{file}.png').crop((offset,0,offset+32,32));
+
+	def draw(self,welded,rotate=0):
+		if rotate==1:
+			rotate=3
+		if rotate==2:
+			rotate=0
+		top,left,bottom,right=rotatewelded(welded,rotate)
+		im=PIL.Image.new('RGBA',(16,16),(0,0,0,0))
+		for x,xside in [(0,left),(8,right)]:
+			for y,yside in [(0,top),(8,bottom)]:
+				im.alpha_composite(self.image.crop((x+16*xside,y+16*yside,x+16*xside+8,y+16*yside+8)),(x,y))
+		im=rotateblock(im,rotate)
+		return im.resize((128,128),PIL.Image.NEAREST)
+
+class NoWeldBlock:
+	def __init__(self,file):
+		self.image=PIL.Image.open(f'blocks/noweld/{file}.png').crop((0,0,16,16));
+
+	def draw(self,_,rotate=0):
+		im=PIL.Image.new('RGBA',(16,16),(0,0,0,0))
+		im.alpha_composite(self.image.crop((0,0,16,16)))
+		return im.resize((128,128),PIL.Image.NEAREST)
+
+class WaferBlock:
+	def __init__(self,file,base='wafer',offset=0):
+		self.wafer=PIL.Image.open(f'blocks/{base}.png').crop((0,0,32,32));
+		self.image=PIL.Image.open(f'blocks/{base}/{file}.png').crop((offset,0,offset+32,32));
+
+	def draw(self,welded,rotate=0,offset=(0,0)):
+		top,left,bottom,right=rotatewelded(welded,rotate)
+		im=PIL.Image.new('RGBA',(16,16),(0,0,0,0))
+		for x,xside in [(0,left),(8,right)]:
+			for y,yside in [(0,top),(8,bottom)]:
+				im.alpha_composite(self.wafer.crop((x+16*xside,y+16*yside,x+16*xside+8,y+16*yside+8)),(x,y))
+		im.alpha_composite(self.image.crop((16*offset[0],16*offset[1],16*(offset[0]+1),16*(offset[1]+1))).rotate(90*rotate))
+		#im=rotateblock(im,rotate)
+		return im.resize((128,128),PIL.Image.NEAREST)
+
+'''
+x=NormalBlock('residue')
+i=x.draw([False,True,True,False])
+i.show()
+
+x=NormalBlock('arc_furnace')
+i=x.draw([True,True,True,False])
+i.show()
+
+x=NormalBlock('arc_furnace',32)
+i=x.draw([False,True,True,False])
+i.show()
+
+x=WaferBlock('accelerometer')
+i=x.draw([False,True,True,False])
+i.show()
+'''
+
+def normalize(block):
+	if block is None:
+		return {"type":'air',"rotate":0,"weld":'all'}
+	if type(block)==str:
+		return {"type":block,"rotate":0,"weld":"all"}
+	if type(block) in [tuple,list]:
+		out={"type":'air',"rotate":0,"weld":'all'}
+		out.update(dict(zip(block,["type","rotate","weld"])))
+		return out
+	out={"type":'air',"rotate":0,"weld":'all'}
+	out.update(block)
+	return out
+
+def get(vss,xi,yi):
+	if xi<0 or yi<0 or yi>=len(vss):
+		return normalize("air");
+	vs=vss[yi]
+	if(xi>=len(vs)):
+		return normalize("air");
+	return vs[xi]
+
+wafertypes=["accelerometer","capacitor","diode","galvanometer","latch","matcher","potentiometer","sensor","transistor"]
+wiretypes=["detector","port","toggler","trigger","wire"]
+noweldtypes=["copper_ore","iron_ore","pulp","sand","silicon","spawner","telecross"]
+twosidetypes=["wire_spool",'wood']
+
+def makeimage(blocks):
+	xsize=max(map(len,blocks))
+	ysize=len(blocks)
+
+	newblocks=[[normalize("air") for _ in range(xsize)] for _ in range(ysize)]
+	for yi,line in enumerate(blocks):
+		for xi,block in enumerate(line):
+			block=normalize(block)
+			newblocks[yi][xi]=block
+
+	im=PIL.Image.new('RGBA',(128*xsize,128*ysize),(0,0,0,0))
+	for xi in range(xsize):
+		for yi in range(ysize):
+			block=get(newblocks,xi,yi)
+			if block['type']=='air':
+				continue
+			if block['weld']=='all':
+				right=get(newblocks,xi+1,yi)['type']!='air'
+				left=get(newblocks,xi-1,yi)['type']!='air'
+				bottom=get(newblocks,xi,yi+1)['type']!='air'
+				top=get(newblocks,xi,yi-1)['type']!='air'
+				block['weld']=[top,left,bottom,right]
+			if block['type'] in wafertypes:
+				b=WaferBlock(block['type'])
+			elif block['type']=='wire_board':
+				b=WaferBlock('wire','wafer')
+			elif block['type'] in wiretypes:
+				b=WaferBlock(block['type'],'frame')
+			elif block['type'] in twosidetypes:
+				b=TwoSideBlock(block['type'])
+			elif block['type'] in noweldtypes:
+				b=NoWeldBlock(block['type'])
+			else:
+				b=NormalBlock(block['type'])
+			bim=b.draw(block['weld'],block['rotate'])
+			im.alpha_composite(bim,(xi*128,yi*128))
+	return im
+
+blocks=[
+	['iron_bar',{"type":'wire_spool',"rotate":1,"weld":[False,False,True,False]}],
+	['air','iron_bar'],
+]
+im=makeimage(blocks)
+im.show()
+im.save('recipe.png')
+
+blocks=[
+	['cast_iron',{"type":'wire_spool',"rotate":2,"weld":[False,False,True,False]}],
+	['cast_iron','cast_iron'],
+]
+im=makeimage(blocks)
+im.show()
+im.save('frecipe.png')
+
+leftspool={"type":'wire_spool',"rotate":1,"weld":[False,False,False,True]}
+rightspool={"type":'wire_spool',"rotate":1,"weld":[False,True,False,False]}
+
+blocks=[
+	[leftspool,'iron_bar',rightspool],
+	[leftspool,'iron_bar',rightspool],
+	[leftspool,'iron_bar',rightspool],
+]
+im=makeimage(blocks)
+im.show()
+im.save('inductor.png')
+
+im1=makeimage([['wire','glass']])
+im2=makeimage([['wire_board','glass']])
+
+im1.save('E.apng', duration=500, save_all=True, append_images=[im2],loop=0,disposal=0,blend=0)
