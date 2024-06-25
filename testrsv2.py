a661e1909d55249ec625132567aa591d23e6bba1
RbCaViTerminal <robert@robertvail.info>
2024-06-25 12:36:25 -0400

    what do the bits mean anyway?

--- testrsv2.py
+++ testrsv2.py
@@ -18,6 +18,19 @@ for i in range(20,50):
   for j in range(20,50):
     rsvedit.setblock(chs,i,j,[random.randint(0,105),0,0,0,0])
 
+for i in range(256):
+  rsvedit.setblock(chs,(i % 16) * 3,-180 + (i // 16) * 3,[59,i,0,0,0])
+
+for i in range(16):
+  for j,c in enumerate(reversed(bin(i + 16)[3:])):
+    rsvedit.setblock(chs,i * 3,-180 - 3 - j,[54,5 * 16,0,ord(c),0])
+  rsvedit.setblock(chs,i * 3,-180 - 2,[59,1 * 16,0,0,0])
+
+for i in range(16):
+  for j,c in enumerate(reversed(bin(i + 16)[3:])):
+    rsvedit.setblock(chs,-3 - j,-180 + i * 3,[54,10 * 16,0,ord(c),0])
+  rsvedit.setblock(chs,-2,-180 + i * 3,[59,8 * 16,0,0,0])
+
 f = '/home/rvail/Desktop/games/Roody2d demo - spark/Roody2d Demo/content/save_templates/wires'
 
 rsv2.writeall(f,chs)
